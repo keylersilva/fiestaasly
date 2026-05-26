ab1de4dbb0983e17bf4e60ccc1a01e48a166a570
keylersilva-dev <keyler.silva.s@unicorsalud.edu.co>
2026-05-26 12:01:02 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -273,7 +273,7 @@ export default function App() {
       </nav>
 
       {/* Main Content */}
-      <main className="max-w-5xl mx-auto p-4 py-8">
+      <main className="flex-1 flex flex-col justify-center max-w-5xl mx-auto px-4 pt-12 pb-32">
         <AnimatePresence mode="wait">
           {view === 'home' ? (
             <RegistrationView config={config} />
